e3b8cc5f67493b492059a2f3e4dabe7b99573144
VOIek <voiekmb@gmail.com>
2024-07-28 22:03:20 +0200

    Wallpaper changing script & generating colorscheme from wallpaper & waybar config

---
+++ .config/scripts/wallpaper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! pgrep -x "hyprpaper" > /dev/null; then
+  hyprpaper &
+fi
+
+wall_dir=$HOME/.Wallpapers
+wallpapers=("$wall_dir"/*)
+
+wallpaper="${wallpapers[RANDOM % ${#wallpapers[@]}]}"
+
+hyprctl hyprpaper preload $wallpaper &
+hyprctl hyprpaper wallpaper "eDP-1, $wallpaper"
+
+wal -i $wallpaper
+
+ln -sf $HOME/.cache/wal/colors-waybar.css $HOME/.config/waybar/colors-waybar.css
