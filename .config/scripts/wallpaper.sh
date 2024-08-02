9c28ea61f75fd37586066e2288ff14840cb09b4e
VOIek <voiekmb@gmail.com>
2024-08-02 22:04:52 +0200

    Some fixes for waybar

--- .config/scripts/wallpaper.sh
+++ .config/scripts/wallpaper.sh
@@ -18,3 +18,7 @@ ln -sf $HOME/.cache/wal/colors-waybar.css $HOME/.config/waybar/colors-waybar.css
 ln -sf $HOME/.cache/wal/colors-waybar.css $HOME/.config/wlogout/colors.css
 ln -sf $HOME/.cache/wal/colors-rofi.rasi $HOME/.config/rofi/colors-rofi.rasi
 ln -sf $HOME/.cache/wal/colors-swaync.css $HOME/.config/swaync/colors-swaync.css
+
+killall swaync
+
+swaync
