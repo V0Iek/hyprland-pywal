312190af2352460d85f1304d54e09d77d8a96b2e
VOIek <voiekmb@gmail.com>
2024-08-05 11:29:39 +0200

    Wallpaper change reloads swaync

--- .config/scripts/wallpaper.sh
+++ .config/scripts/wallpaper.sh
@@ -19,8 +19,4 @@ ln -sf $HOME/.cache/wal/colors-waybar.css $HOME/.config/wlogout/colors.css
 ln -sf $HOME/.cache/wal/colors-rofi.rasi $HOME/.config/rofi/colors-rofi.rasi
 ln -sf $HOME/.cache/wal/colors-swaync.css $HOME/.config/swaync/colors-swaync.css
 
-swaync-client -R
-
-killall swaync
-
-swaync
+swaync-client -rs
